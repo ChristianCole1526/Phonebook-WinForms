d2a48238c04acd92401c45107dcd98d7c62cce83
ChristianCole1526 <christiancole1526@gmail.com>
2024-10-14 05:17:31 +0300

    v2
    Created a FormHelper class that encapsulates all the validation and refreshing methods and also improved the searching by using LIKE instead of equals.

---
+++ Phonebook/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Phonebook
+{
+    internal class FormHelper
+    {
+
+        public void RefreshDataTable(DataGridView data_grid, SqlConnection connection)
+        {
+            this.ChangeDataSource($"SELECT * FROM People", data_grid, connection);
+        }
+
+
+        public void ChangeDataSource(String query, DataGridView data_grid, SqlConnection conn)
+        {
+            DataSet data_set = new DataSet();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+            {
+                adapter.Fill(data_set);
+                data_grid.DataSource = data_set.Tables[0];
+            }
+        }
+
+        
+        public bool ValidateInputs(String f_name, String l_name, String phone_no)
+        {
+            if (Regex.IsMatch(f_name.Trim(), @"^[a-zA-Z]+$") & Regex.IsMatch(l_name.Trim(), @"^[a-zA-Z]+$") & Regex.IsMatch(phone_no.Trim(), @"^\d+$"))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Input Data Not In Correct Format");
+
+            return false;
+        }
+
+        public bool PersonExists(DataGridView data_grid, SqlConnection conn, string f_name, string l_name, string phone_no)
+        {
+            try
+            {
+                DataSet data_set = new DataSet();
+                using (SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM People WHERE first_name = '{f_name}' AND last_name = '{l_name}' AND phone_number = '{phone_no}'", conn))
+                {
+                    adapter.Fill(data_set);
+                    data_grid.DataSource = data_set.Tables[0].Rows[0];
+
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AllFieldsCompleted(String f_name, String l_name, String phone_no)
+        {
+            if (f_name != "" && l_name != "" && phone_no != "")
+            {
+                return true;
+            }
+
+            MessageBox.Show("All Fields Must Be Completed");
+
+            return false;
+        }
+
+    }
+}
--- Phonebook/Form1.cs
+++ Phonebook/Form1.cs
@@ -1,145 +1,57 @@
 ﻿using System;
-using System.Data;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data;
+using System.Xml.Linq;
 
 namespace Phonebook
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
         SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-0AJBLM0\SQLEXPRESS;Initial Catalog=Phonebook;Integrated Security=True");
+        FormHelper form_helper = new FormHelper();
         String first_name = "";
         String last_name = "";
         String phone_number = "";
 
+        public Form1()
+        {
+            InitializeComponent();
+            connection.Open();
+        }
+
+ 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.RefreshDataTable();
+            form_helper.RefreshDataTable(dataGridView1, connection);
 
-        }
-
-        public void RefreshDataTable()
-        {
-            DataSet data_set = new DataSet();
-            using (SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM People", connection))
-            {
-                adapter.Fill(data_set);
-                dataGridView1.DataSource = data_set.Tables[0];
-            }
-        }
-
-        public bool ValidateInputs()
-        {
-            try
-            {
-                first_name.ToString();
-                last_name.ToString();
-
-                if (phone_number != "")
-                {
-                    Int64.Parse(phone_number);
-                }
-
-                return true;
-
-            } catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-
-                return false;
-            }
-        }
-
-        private bool PersonExists()
-        {
-            try
-            {
-                DataSet data_set = new DataSet();
-                using (SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM People WHERE first_name = '{first_name}' AND last_name = '{last_name}' AND phone_number = '{phone_number}'", connection))
-                {
-                    adapter.Fill(data_set);
-                    dataGridView1.DataSource = data_set.Tables[0].Rows[0];
-
-                    return true;
-                }
-            } catch {
-                return false;
-            }
-        }
-
-        private bool AllFieldsCompleted()
-        {
-            if (first_name != "" && last_name != "" && phone_number != "")
-            {
-                return true;
-            } 
-            MessageBox.Show("All Fields Must Be Completed");
-
-            return false;
-            
         }
 
         private void FindRecordBtn_Click(object sender, EventArgs e)
         {
-            String select_query = $"SELECT * FROM People WHERE ";
+            String select_query = $"SELECT * FROM People WHERE first_name LIKE '{first_name}%' AND last_name LIKE '{last_name}%' AND phone_number LIKE '{phone_number}%'";
 
-            
-            if (first_name != "")
-            {
-                select_query += $"first_name = '{first_name}' AND ";
-            } 
-
-            if (last_name != "")
-            {
-                select_query += $"last_name = '{last_name}' AND ";
-            }
-
-            if (phone_number != "")
-            {
-                select_query += $"phone_number = '{phone_number}' AND ";
-            }
-
-
-            select_query = select_query.Trim();
-            select_query = select_query.Remove(select_query.LastIndexOf(' '));
-
-            if (this.ValidateInputs())
-            {
-                DataSet data_set = new DataSet();
-                using (SqlDataAdapter adapter = new SqlDataAdapter(select_query, connection))
-                {
-                    adapter.Fill(data_set);
-                    dataGridView1.DataSource = data_set.Tables[0];
-                }
-            }
+            form_helper.ChangeDataSource(select_query, dataGridView1, connection);
         }
+
 
         private void DeleteRecordBtn_Click(object sender, EventArgs e)
         {
 
-            if (this.AllFieldsCompleted() && this.ValidateInputs())
+            if (form_helper.AllFieldsCompleted(first_name, last_name, phone_number) && form_helper.ValidateInputs(first_name, last_name, phone_number))
             {
-                if (this.PersonExists())
+                if (form_helper.PersonExists(dataGridView1, connection, first_name, last_name, phone_number))
                 {
                     String delete_query = $"DELETE FROM People WHERE first_name = '{first_name}' AND last_name = '{last_name}' AND phone_number = '{phone_number}'";
                     SqlCommand insert_command = new SqlCommand(delete_query, connection);
                     SqlDataReader sql_reader;
 
-                    connection.Open();
-
                     sql_reader = insert_command.ExecuteReader();
                     sql_reader.Close();
 
-                    this.RefreshDataTable();
+                    form_helper.RefreshDataTable(dataGridView1, connection);
 
                     MessageBox.Show($"Person: {first_name} {last_name} Deleted Succesfully");
-
-                    connection.Close();
                 }
                 else
                 {
@@ -148,28 +60,28 @@ namespace Phonebook
             }
         }
 
+
         private void AddRecordBtn_Click(object sender, EventArgs e)
         {
-            if (this.AllFieldsCompleted() && this.ValidateInputs()) 
+            if (form_helper.AllFieldsCompleted(first_name, last_name, phone_number) && form_helper.ValidateInputs(first_name, last_name, phone_number)) 
             {
-                if (!this.PersonExists())
+                if (!form_helper.PersonExists(dataGridView1, connection, first_name, last_name, phone_number))
                 {
                     String add_query = $"INSERT INTO People (first_name, last_name, phone_number) VALUES('{first_name}', '{last_name}', '{phone_number}')";
                     SqlCommand insert_command = new SqlCommand(add_query, connection);
                     SqlDataReader sql_reader;
 
-                    connection.Open();
-
                     sql_reader = insert_command.ExecuteReader();
                     sql_reader.Close();
 
                     MessageBox.Show($"Person: {first_name} {last_name} Entered Succesfully");
-
-                    connection.Close();
-                } else {
+                } 
+                else
+                {
                     MessageBox.Show("Person Already Exists In Table");
                 }
-                this.RefreshDataTable();
+
+                form_helper.RefreshDataTable(dataGridView1, connection);
             }
         }
 
@@ -178,19 +90,22 @@ namespace Phonebook
             phone_number = PhoneNumberInput.Text;
         }
 
+
         private void LastNameInput_TextChanged(object sender, EventArgs e)
         {
             last_name = LastNameInput.Text;
         }
+
 
         private void FirstNameInput_TextChanged(object sender, EventArgs e)
         {
             first_name = FirstNameInput.Text;
         }
 
+
         private void RefreshTableBtn_Click(object sender, EventArgs e)
         {
-            this.RefreshDataTable();
+            form_helper.RefreshDataTable(dataGridView1, connection);
         }
     }
 }
